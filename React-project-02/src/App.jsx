fc34c0e12b37d92dfda5f115a9f81bf5e46827e9
Sumit Sharma <sumitsharma23803@gmail.com>
2026-02-27 23:44:40 +0530

    modify app.jsx

--- React-project-02/src/App.jsx
+++ React-project-02/src/App.jsx
@@ -10,6 +10,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/setup/proprietorship" element={<Proprietorship />} />
+           <Route path="/setup/unregisterd-partnership" element={<Proprietorship />} />
         </Routes>
       </Layout>
     </Router>
